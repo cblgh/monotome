47933109b9725fe4a550cd1577f65c604f120e22
Alexander Cobleigh <cblgh@cblgh.org>
2018-09-23 18:33:49 +0200

    fix analytics

--- monotome/bin/analytics.js
+++ monotome/bin/analytics.js
@@ -1,26 +1,34 @@
 var fs = require("fs")
-
-// TODO: rewrite using streams 
-var index = `${process.cwd()}/index.json`
-fs.readFile(index, function(err, data) {
+fs.readFile(`${process.cwd()}/index.json`, function(err, data) {
+    if (err) throw err
     var index = JSON.parse(data)
     var promises = []
+    // count subjects
     var subjects = Object.keys(index.subjects)
-    console.log(subjects)
+    // count files
     var filecount = subjects.map((s) => index.subjects[s].length).reduce((a, c) => a+c)
+    // count words
     subjects.forEach((subject) => {
-        promises = index.subjects[subject].map((file) => {
+        subjectPromises = index.subjects[subject].map((file) => {
             return new Promise((resolve, reject) => {
                 fs.readFile(`${process.cwd()}/${subject}/${file}`, (err, data) => {
                     if (err) reject(err)
-                    resolve(data.toString().split(/ /g).length)
+                    resolve({ 
+                        words: data.toString().split(/ /g).length, 
+                        chars: data.toString().split(/./g).length 
+                    })
                 })
             })
         })
+        promises = promises.concat(subjectPromises)
     })
     Promise.all(promises).then((results) => {
+        var count = results.reduce((acc, cur) => {
+            return { words: acc.words + cur.words, chars: acc.chars + cur.chars }
+        }, {words: 0, chars: 0})
         console.log("subjects:", subjects.length)
         console.log("files:   ", filecount)
-        console.log("words:   ", results.reduce((acc, cur) => acc+cur))
+        console.log("words:   ", count.words) 
+        console.log("chars:   ", count.chars) 
     })
 })
