442ac6cb76a70e0099dd4048623cc47b2e48f46f
cblgh <cblgh@cblgh.org>
2019-02-08 13:53:07 +0100

    add host to dom title
    to show the current host of an article you can append the host domain to
    `document.title` using `window.location.host`.

--- monotome/scripts/instance.js
+++ monotome/scripts/instance.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+    document.title += " | " + window.location.host
+
     read(source, function (body) {
         document.querySelector(".content").innerHTML = body
     })
