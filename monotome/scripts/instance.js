8a8fd10d1d2e3aefe00406be0dbaa40ba2fb7b35
cblgh <cblgh@cblgh.org>
2019-02-22 16:56:42 +0100

    feat: add article index, improve mobile layout

--- monotome/scripts/instance.js
+++ monotome/scripts/instance.js
@@ -1,5 +1,12 @@
 window.onload = function () {
     document.title += " | " + window.location.host
+    var header = document.createElement("h1")
+    header.classList.add("index-link")
+    var link = document.createElement("a")
+    link.setAttribute("href", "/articles")
+    link.textContent = "Index"
+    header.appendChild(link)
+    document.body.prepend(header)
 
     read(source, function (body) {
         document.querySelector(".content").innerHTML = body
